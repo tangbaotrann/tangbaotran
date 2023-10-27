6a9d70563a8243d92f2a08e0b98cd23007e9341b
tangbaotrann <tangbaotrann@gmail.com>
2023-10-28 02:33:38 +0700

    updated: done swap

--- src/components/CurrencySwapForm/Buttons/index.js
+++ src/components/CurrencySwapForm/Buttons/index.js
@@ -3,11 +3,11 @@ import { Button } from "antd";
 function Buttons({ handleResetForm }) {
   return (
     <div>
-      <Button className="btn-reset" onClick={handleResetForm}>
-        Làm mới
-      </Button>
       <Button type="primary" htmlType="submit">
         Chuyển đổi
+      </Button>
+      <Button className="btn-reset" onClick={handleResetForm}>
+        Làm mới
       </Button>
     </div>
   );
